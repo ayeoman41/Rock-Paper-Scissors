a541fd190161c1675ff3dbd325aa9c14e0dedd62
Beez <ayeoman41@gmail.com>
2022-12-17 12:00:10 -0500

    added playRound function

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function getComputerChoice(){
 }
 
 getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Paper"){
+        return ("You lose! Rock beats paper");
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        return ("You lose! Scissors beats paper");
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+        return ("You lose! Rock beats paper");
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+        return ("You Win! Scissors beats paper");
+    } else if (playerSelection == "Paper" && computerSelection == "Rock"){
+        return ("You win! Paper beats rock");
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        return ("You win! Scissors beats paper");
+    } else {
+        return ("Tie")
+    }
+}
