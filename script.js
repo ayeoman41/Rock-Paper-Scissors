ec1e8bafc4414bf2eee67baf6d1443fcabe8e58b
dadixon <drocdix7@gmail.com>
2022-12-28 13:49:14 -0500

    dom suggestions Many ways to skin a cat but here's what I think

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-let num = prompt("Type weapon number choice: 1=Rock, 2=Paper, 3=Scissors")
-const choice = ['Rock', 'Paper', 'Scissors'];
-const random = Math.floor(Math.random() *choice.length);
-const randomChoice = choice[random];
-
-
-function getComputerChoice(){
-    console.log(randomChoice)
+let num = prompt("Type weapon number choice: 1=Rock, 2=Paper, 3=Scissors");
+if (isNaN(num) ||
+    (num > 3 || num < 1)
+) {
+    alert("Please enter an number between 1-3");
 }
 
-function player(num) {
-    if (num == 1) {
-        return choice[0]
-    } else if (num == 2) {
-        return choice[1]
-    } else (num == 3) ;
-        return choice[2]
-    }
+const choice = ['Rock', 'Paper', 'Scissors'];
+console.log('You chose: ' + choice[num - 1]);
 
-const playerSelection = player()
-const computerSelection = getComputerChoice
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection == "Rock" && computerSelection == "Paper"){
-            return ("You lose! Rock beats paper");
-        } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
-            return ("You lose! Scissors beats paper");
-        } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
-            return ("You lose! Rock beats paper");
-        } else if (playerSelection == "Rock" && computerSelection == "Scissors"){
-            return ("You Win! Scissors beats paper");
-        } else if (playerSelection == "Paper" && computerSelection == "Rock"){
-            return ("You win! Paper beats rock");
-        } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
-            return ("You win! Scissors beats paper");
-        } else (playerSelection === computerSelection)
-            return ("Tie")
+const playerSelection = choice[num - 1];
+const random = Math.floor(Math.random() * choice.length);
+const computerSelection = choice[random];
+console.log('Opponent chose: ' + computerSelection);
+
+console.log(playRound(num - 1, random));
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    } else if (playerSelection > computerSelection) {
+        if (computerSelection == 0 && playerSelection == choice.length - 1) {
+            return "You lose! " + choice[computerSelection] + " beats " + choice[playerSelection];
         }
+        return "You win! " + choice[playerSelection] + " beats " + choice[computerSelection];
+    } else if (computerSelection == choice.length - 1 && playerSelection == 0) {
+        return "You win! " + choice[playerSelection] + " beats " + choice[computerSelection];
+    } else if (computerSelection > playerSelection) {
+        if (playerSelection == 0 && computerSelection == choice.length - 1) {
+            return "You win! " + choice[playerSelection] + " beats " + choice[computerSelection];
+        }
+        return "You lose! " + choice[computerSelection] + " beats " + choice[playerSelection];
+    } else if (playerSelection == choice.length - 1 && computerSelection == 0) {
+        return "You lose! " + choice[computerSelection] + " beats " + choice[playerSelection];
+    }
+}
 
-console.log(playRound(player(num), computerSelection))
+// Unit tests
+console.assert(playRound(0, 0) === "Tie", "This should be a tie");
+console.assert(playRound(1, 1) === "Tie", "This should be a tie");
+console.assert(playRound(2, 2) === "Tie", "This should be a tie");
+console.assert(playRound(0, 2) === "You win! Rock beats Scissors", "Rock beats scissors");
+console.assert(playRound(2, 0) === "You lose! Rock beats Scissors", "Rock beats scissors");
+console.assert(playRound(0, 1) === "You lose! Paper beats Rock", "Paper beats rock");
+console.assert(playRound(1, 0) === "You win! Paper beats Rock", "Paper beats rock");
+console.assert(playRound(1, 2) === "You lose! Scissors beats Paper", "Scissors beats paper");
+console.assert(playRound(2, 1) === "You win! Scissors beats Paper", "Scissors beats paper");
